db9c2680565f88cb8dbc62ca1b7338f5157e082b
Rahul <kumrahulsingh7575@gmail.com>
2026-02-01 17:28:36 +0530

    changed upload files

--- improved_dashboard.py
+++ improved_dashboard.py
@@ -5,12 +5,14 @@ import google.generativeai as genai
 st.set_page_config(page_title="AI Analyst",page_icon="🤖")
 st.title(" 200k views AI dashbard")
 api_key= st.sidebar.text_input("Enter google api key",type="password")
-df=pd.read_excel("api_fetched.xlsx",engine="openpyxl")
-unique_account=df['Account_Name'].unique()
-option=st.sidebar.selectbox("select the account type",unique_account)
-filtered_df=df[df['Account_Name']==option]
-st.subheader(f"Data Preview for {option}")
-st.dataframe(filtered_df)
+uploaded_file= st.file_uploader("Upload your Excel file",type=["xlsx"])
+if uploaded_file:
+    df=pd.read_excel(uploaded_file,engine="openpyxl")
+    unique_account=df['Account_Name'].unique()
+    option=st.sidebar.selectbox("select the account type",unique_account)
+    filtered_df=df[df['Account_Name']==option]
+    st.subheader(f"Data Preview for {option}")
+    st.dataframe(filtered_df)
 
 if api_key:
     genai.configure(api_key=api_key)
